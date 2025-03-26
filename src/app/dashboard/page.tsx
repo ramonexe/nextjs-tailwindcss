da4780ccc1c08c77bf085c63333dd86ba2380f64
Ramon Garcia <ramonsantos.g@hotmail.com>
2025-03-26 16:26:07 -0300

    adiciona página de dashboard e atualiza página inicial

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export const metadata = {
-    title: "Projeto - Inicio",
+    title: "Projeto - Dashboard",
     description: "Essa é a descrição da minha página para SEO",
 };
 
@@ -9,11 +9,11 @@ export default function Home() {
     return (
         <div className="flex flex-col min-h-screen">
             <header className="py-4">
-                <h1 className="text-center text-2xl font-bold">Página Inicial</h1>
+                <h1 className="text-center text-2xl font-bold">Dashboard</h1>
             </header>
             <main className="flex flex-1 flex-col items-center justify-center">
-                <Link href="/dashboard" className="bg-white hover:bg-sky-700 duration-100 ease-in text-black rounded-xl p-2 font-semibold">
-                    Dashboard
+                <Link href="/" className="bg-white hover:bg-sky-700 duration-100 ease-in text-black rounded-xl p-2 font-semibold">
+                    Página Inicial
                 </Link>
             </main>
         </div>
